dfcfc58abcb6e61969a0bd51959d0cff655385c7
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-29 15:53:17 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+rows = int (input("PLEASE ENTER THE TOTAL NUMBER OF ROWS :"))
+number  = 1
+print("FLOYDS TRIANGLE")
+for i in range(1,rows + 1):
+    print(number, end = ' ')
+    number = number + 1
+    print()
